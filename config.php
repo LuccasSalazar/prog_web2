f909cd92b2b20a810fd2e62fb0a371625768c2f2
LuccasSalazar <salazarllucas@gmail.com>
2022-10-26 08:38:48 -0500

    consertei tudo

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+    $bd_servidor = "localhost";
+    $bd_name = "cadastro";
+    $bd_usuario = "root";
+    $$bd_senha = "";
+
+    $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $bd_name);
+
+    if(!$conexao)
+      die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    echo "Conexão realizada com sucesso!";
+    //Conetando com o bando de dados
+
+    //Inserindo dados no banco
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $sexo = $_POST['genero'];
+    $data_nascimento = $_POST['data_nascimento'];
+    $estado = $_POST['estado'];
+    $cidade = $_POST['cidade'];
+    $endereco = $_POST['endereco'];
+
+
+    $sql = "INSERT INTO usuarios(nome, senha, email, telefone, genero, data_nascimento, estado, cidade, endereco) VALUES ('$nome', '$senha', '$email', '$telefone', '$sexo', '$data_nascimento', '$estado', '$cidade', '$endereco')";
+
+    if(mysqli_query($conexao, $sql))
+        echo 'Cadastrado  com sucesso';
+    else    
+        echo "Erro!";
+
+    mysqli_close($conexao);
+?>
